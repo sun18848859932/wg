265c865c2e9895991446f285003e55055e7f9136
syh <767884144@qq.com>
2019-12-05 15:33:34 +0800

    33333

--- src/wg/dgjj.java
+++ src/wg/dgjj.java
@@ -4,5 +4,6 @@ public class dgjj {
 	public static void main(String[] args) {
 		System.out.println("11111111111");
 		System.out.println("11111111111");
+		System.out.println("11111111111");
 	}
 }
